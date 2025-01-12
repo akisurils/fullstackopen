ad85d40cf1d83165a42619fbca3bdb5558624e3b
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-12 15:40:33 -0500

    Implement country lookup

--- part2/countrydata/src/App.jsx
+++ part2/countrydata/src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const CountryInputForm = ({ country, setCountry }) => {
+  const handleInputChange = (event) => {
+    // console.log(event.target.value);
+    setCountry(event.target.value);
+  };
+  return (
+    <div>
+      Find countries:{" "}
+      <input
+        type="text"
+        placeholder="United State"
+        onChange={handleInputChange}
+        value={country}
+      />
+    </div>
+  );
+};
+
+const SearchResult = ({ country, countryList }) => {
+  const matchedCountries = countryList.filter((ct) => {
+    return ct.name.common.toLowerCase().search(country.toLowerCase()) != -1;
+  });
+  console.log(matchedCountries);
+  if (countryList.length == 0) {
+    return <div>Loading, please wait...</div>;
+  }
+
+  if (matchedCountries.length == 1) {
+    const matchedCountry = matchedCountries[0];
+    return (
+      <div>
+        {matchedCountry.name.common}
+        <h1>{matchedCountry.name.common}</h1>
+        <div>Capital: {matchedCountry.capital[0]}</div>
+        <div>Area: {matchedCountry.area} km2</div>
+        <h3>Languages:</h3>
+        <ul>
+          {Object.values(matchedCountry.languages).map((l) => (
+            <li>{l}</li>
+          ))}
+        </ul>
+        <img src={matchedCountry.flags.svg} alt={matchedCountry.flags.alt} />
+      </div>
+    );
+  }
+
+  if (matchedCountries.length <= 10) {
+    return (
+      <div>
+        {matchedCountries.map((ct, id) => {
+          return <div key={id}>{ct.name.common}</div>;
+        })}
+      </div>
+    );
+  }
+
+  return <div></div>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [country, setCountry] = useState("");
+  const [countryList, setCountryList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((response) => {
+        console.log(response.data);
+        setCountryList(response.data);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CountryInputForm country={country} setCountry={setCountry} />
+      <SearchResult country={country} countryList={countryList} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
