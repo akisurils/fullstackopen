1c6368d61b2ac2303e575f7ef9e54ebcdfdeef44
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-12 16:03:16 -0500

    Add single country view

--- part2/countrydata/src/App.jsx
+++ part2/countrydata/src/App.jsx
@@ -19,39 +19,63 @@ const CountryInputForm = ({ country, setCountry }) => {
   );
 };
 
-const SearchResult = ({ country, countryList }) => {
+const CountryDetail = ({ country }) => {
+  return (
+    <div>
+      {country.name.common}
+      <h1>{country.name.common}</h1>
+      <div>Capital: {country.capital[0]}</div>
+      <div>Area: {country.area} km2</div>
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((l, id) => (
+          <li key={id}>{l}</li>
+        ))}
+      </ul>
+      <img width={100} src={country.flags.svg} alt={country.flags.alt} />
+    </div>
+  );
+};
+
+const SearchResult = ({
+  country,
+  countryList,
+  singleCountryView,
+  setSingleCountryView,
+}) => {
   const matchedCountries = countryList.filter((ct) => {
     return ct.name.common.toLowerCase().search(country.toLowerCase()) != -1;
   });
+
   console.log(matchedCountries);
   if (countryList.length == 0) {
     return <div>Loading, please wait...</div>;
   }
 
+  if (singleCountryView.isViewing) {
+    return <div></div>;
+  }
+
   if (matchedCountries.length == 1) {
     const matchedCountry = matchedCountries[0];
-    return (
-      <div>
-        {matchedCountry.name.common}
-        <h1>{matchedCountry.name.common}</h1>
-        <div>Capital: {matchedCountry.capital[0]}</div>
-        <div>Area: {matchedCountry.area} km2</div>
-        <h3>Languages:</h3>
-        <ul>
-          {Object.values(matchedCountry.languages).map((l) => (
-            <li>{l}</li>
-          ))}
-        </ul>
-        <img src={matchedCountry.flags.svg} alt={matchedCountry.flags.alt} />
-      </div>
-    );
+    return <CountryDetail country={matchedCountry} />;
   }
+
+  const handleSingleCountryView = (ct) => {
+    const newView = { cca2: ct.cca2, isViewing: true };
+    setSingleCountryView(newView);
+  };
 
   if (matchedCountries.length <= 10) {
     return (
       <div>
         {matchedCountries.map((ct, id) => {
-          return <div key={id}>{ct.name.common}</div>;
+          return (
+            <div key={id}>
+              {ct.name.common}{" "}
+              <button onClick={() => handleSingleCountryView(ct)}>show</button>
+            </div>
+          );
         })}
       </div>
     );
@@ -60,9 +84,38 @@ const SearchResult = ({ country, countryList }) => {
   return <div></div>;
 };
 
+const SingleCountryView = ({
+  countryList,
+  singleCountryView,
+  setSingleCountryView,
+}) => {
+  if (singleCountryView.isViewing) {
+    return (
+      <div>
+        <CountryDetail
+          country={countryList.find((ct) => ct.cca2 == singleCountryView.cca2)}
+        />
+        <button
+          onClick={() => {
+            const newView = { cca2: null, isViewing: false };
+            setSingleCountryView(newView);
+          }}
+        >
+          back
+        </button>
+      </div>
+    );
+  }
+  return <div></div>;
+};
+
 function App() {
   const [country, setCountry] = useState("");
   const [countryList, setCountryList] = useState([]);
+  const [singleCountryView, setSingleCountryView] = useState({
+    cca2: null,
+    isViewing: false,
+  });
 
   useEffect(() => {
     axios
@@ -76,7 +129,17 @@ function App() {
   return (
     <>
       <CountryInputForm country={country} setCountry={setCountry} />
-      <SearchResult country={country} countryList={countryList} />
+      <SearchResult
+        country={country}
+        countryList={countryList}
+        singleCountryView={singleCountryView}
+        setSingleCountryView={setSingleCountryView}
+      />
+      <SingleCountryView
+        countryList={countryList}
+        singleCountryView={singleCountryView}
+        setSingleCountryView={setSingleCountryView}
+      />
     </>
   );
 }
