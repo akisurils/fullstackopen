1012ab9f0e40dd8db6abb81d40c05e3c57239a5f
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-16 18:45:39 -0500

    Add mongodb with test connection

---
+++ demo/intronode/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://akisuri:${password}@fullstackopen.tehbi.mongodb.net/noteApp?retryWrites=true&w=majority&appName=fullstackopen`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "Mongoose make thing easy",
+  important: true,
+});
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   console.log(result);
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
