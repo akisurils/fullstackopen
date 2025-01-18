04e0f0253c10ee915ea03d215bca0b166dc858a9
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-17 21:26:53 -0500

    Add mongodb connect and endpoint

--- demo/intronode/mongo.js
+++ demo/intronode/mongo.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
   console.log("give password as argument");
+  return;
 }
 
 const password = process.argv[2];
@@ -30,7 +31,7 @@ const note = new Note({
 //   mongoose.connection.close();
 // });
 
-Note.find({}).then((result) => {
+Note.find({ important: true }).then((result) => {
   result.forEach((note) => {
     console.log(note);
   });
