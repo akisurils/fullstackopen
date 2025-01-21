10b28ff985ce539b2c4235e3f61b0234c1e4640e
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-20 23:20:39 -0500

    Integrate mongodb to backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,128 +1,134 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
 app.use(cors());
 
+const url = process.env.VITE_MONGODB_URL;
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
 morgan.token("reqbody", (request, response) => {
-  return JSON.stringify(request.body);
+    return JSON.stringify(request.body);
 });
 app.use(
-  morgan(
-    ":method :url :status :req[content-length] - :response-time ms :reqbody"
-  )
+    morgan(
+        ":method :url :status :req[content-length] - :response-time ms :reqbody"
+    )
 );
 
 let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+    {
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
 ];
 
 app.get("/info", (request, response) => {
-  const currentDate = Date(Date.now);
-  response.send(
-    `<div>Phonebook have infomation of ${
-      persons.length
-    } people</div> <br /> <div>${currentDate.toString()}</div>`
-  );
+    const currentDate = Date(Date.now);
+    response.send(
+        `<div>Phonebook have infomation of ${
+            persons.length
+        } people</div> <br /> <div>${currentDate.toString()}</div>`
+    );
 });
 
 app.get("/api/persons", (request, response) => {
-  response.send(persons);
+    response.send(persons);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+    const id = request.params.id;
+    const person = persons.find((person) => person.id === id);
 
-  if (!person) {
-    return response.status(404).end("404 not found");
-  }
+    if (!person) {
+        return response.status(404).end("404 not found");
+    }
 
-  return response.send(person);
+    return response.send(person);
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+    const id = request.params.id;
+    const person = persons.find((person) => person.id === id);
 
-  if (!person) {
-    return response.status(404).end("404 not found");
-  }
+    if (!person) {
+        return response.status(404).end("404 not found");
+    }
 
-  persons = persons.filter((p) => p.id !== id);
-  return response.status(204).end();
+    persons = persons.filter((p) => p.id !== id);
+    return response.status(204).end();
 });
 
 app.post("/api/persons", (request, response) => {
-  const body = request.body;
-  const name = body.name;
-  const number = body.number;
-  const id = body.id;
+    const body = request.body;
+    const name = body.name;
+    const number = body.number;
+    const id = body.id;
 
-  if (!name) {
-    return response.status(400).end("missing name");
-  }
+    if (!name) {
+        return response.status(400).end("missing name");
+    }
 
-  if (!number) {
-    return response.status(400).end("missing number");
-  }
+    if (!number) {
+        return response.status(400).end("missing number");
+    }
 
-  if (persons.find((person) => person.name === name)) {
-    return response.status(400).end("name must be unique");
-  }
+    if (persons.find((person) => person.name === name)) {
+        return response.status(400).end("name must be unique");
+    }
 
-  if (persons.find((person) => person.id === id)) {
-    return response.status(400).end("id must be unique");
-  }
+    if (persons.find((person) => person.id === id)) {
+        return response.status(400).end("id must be unique");
+    }
 
-  newPerson = { name, number, id };
-  persons = persons.concat(newPerson);
-  return response.json(newPerson);
+    newPerson = { name, number, id };
+    persons = persons.concat(newPerson);
+    return response.json(newPerson);
 });
 
 app.put("/api/persons/:id", (request, response) => {
-  const body = request.body;
-  const name = body.name;
-  const number = body.number;
-  const id = body.id;
+    const body = request.body;
+    const name = body.name;
+    const number = body.number;
+    const id = body.id;
 
-  if (!name) {
-    return response.status(400).end("missing name");
-  }
+    if (!name) {
+        return response.status(400).end("missing name");
+    }
 
-  if (!number) {
-    return response.status(400).end("missing number");
-  }
+    if (!number) {
+        return response.status(400).end("missing number");
+    }
 
-  newPerson = { name, number, id };
-  persons = persons.map((p) => (p.id === id ? newPerson : p));
-  return response.json(newPerson);
+    newPerson = { name, number, id };
+    persons = persons.map((p) => (p.id === id ? newPerson : p));
+    return response.json(newPerson);
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+    response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
