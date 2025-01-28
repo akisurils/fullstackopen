41c1273ff259661051b4c965198019de5ffa1618
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-28 18:06:28 -0500

    feat: Add and configure eslint

---
+++ demo/intronode/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            globals: { ...globals.node },
+            ecmaVersion: "latest",
+        },
+    },
+    {
+        plugins: {
+            "@stylistic/js": stylisticJs,
+        },
+        rules: {
+            "@stylistic/js/indent": ["error", 4],
+            "@stylistic/js/linebreak-style": ["error", "windows"],
+            "@stylistic/js/quotes": ["error", "double"],
+            "@stylistic/js/semi": ["error", "always"],
+        },
+    },
+];
