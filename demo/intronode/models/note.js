9cab3a633bdbf408079e8bd026f6fc32c99c609d
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-30 16:11:28 -0500

    feat: update more linting rule

--- demo/intronode/models/note.js
+++ demo/intronode/models/note.js
@@ -6,7 +6,7 @@ const url = process.env.VITE_MONGODB_URI;
 
 mongoose
     .connect(url)
-    .then((result) => {
+    .then(() => {
         console.log("Connected to MongoDB");
     })
     .catch((error) => {
